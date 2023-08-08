6e31ff0c6a942414fb5e9e2322ea2877317c3fd4
CharlotteBimou1 <50669298+CharlotteBimou1@users.noreply.github.com>
2023-08-08 12:45:55 +0200

    Add files via upload

--- seg_client.py
+++ seg_client.py
@@ -3,14 +3,22 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
+logo_image = "pplink.png"  # Replace with the path to your logo image
+st.image(logo_image, width=150)
+
 # Perform query.
+# Utiliser la balise HTML <div> avec le style CSS pour justifier le texte
 st.header('Segmentation de la clientèle par K-Means (Algorithme de Machine Learning non supervisé)')
-
-st.write('Échantillon de données en grappes(groupe) montrant la récurrence, la fréquence et les attributs monétaires de chaque client')
-
-st.write('Source des données utilisées : https://archive.ics.uci.edu/dataset/352/online+retail')
-st.write('Informations complémentaires des données:')
-st.write('Ensemble de données transnationales qui contient toutes les transactions effectuées entre le 01/12/2010 et le 09/12/2011 pour un commerce de détail en ligne non-magasin basé au Royaume-Uni et enregistré. De nombreux clients de cette entreprise sont des grossistes.')
+st.write("Objectif de l'application : permettre aux équipes opérationnelles et techniques "
+         "de diviser leur base de clients, tant les clients existants que les nouveaux, afin de leur "
+         "offrir de nouvelles prestations et d'entreprendre des efforts de prospection auprès de nouveaux "
+         "clients potentiels.")
+st.write('**Source des données utilisées**: https://archive.ics.uci.edu/dataset/352/online+retail')
+st.write('**Informations complémentaires des données**:')
+st.write('Ensemble de données transnationales qui contient toutes les transactions effectuées entre le '
+         '01/12/2010 et le 09/12/2011 pour un commerce de détail en ligne non-magasin basé au Royaume-Uni '
+         'et enregistré. De nombreux clients de cette entreprise sont des grossistes.')
+st.write('**Affichage** : Échantillon de données en grappes(groupe) montrant la récurrence, la fréquence et les attributs monétaires de chaque client')
 
 @st.cache_data
 def get_data():
@@ -27,7 +35,7 @@ def main():
     col1.dataframe(df_pd)
 
     # Move the graphs to the right column
-    col2.subheader('Fréquence versus Récence')
+    col2.subheader('Fréquence des clients en fonction de leurs récences sur le site e-commerce')
     fig = px.scatter(
         df_pd,
         x="Frequency",
@@ -37,7 +45,7 @@ def main():
     )
     col2.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
-    col2.subheader('Fréquence versus Monétaire')
+    col2.subheader('Fréquence des clients en fonction de leurs dépenses monétaire')
     fig = px.scatter(
         df_pd,
         x="Frequency",
@@ -47,7 +55,7 @@ def main():
     )
     col2.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
-    col2.subheader('Récense versus Monétaire')
+    col2.subheader('Récense des clients sur le site e-commerce en fonction de leurs dépenses')
     fig = px.scatter(
         df_pd,
         x="RECENCY",
